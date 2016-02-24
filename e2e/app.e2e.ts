4b269fc5eaa7a766509b11a0aa1a688b3a5fc0c2
Tomster <tomster@emberjs.com>
2016-02-24 08:52:48 -0500

    Initial Commit from Ember CLI v1.13.15

---
+++ e2e/app.e2e.ts
@@ -0,0 +1,16 @@
+/// <reference path="../typings/main.d.ts" />
+
+import { RhythmosPage } from './app.po';
+
+describe('rhythmos App', function() {
+  let page: RhythmosPage;
+
+  beforeEach(() => {
+    page = new RhythmosPage();
+  })
+
+  it('should display message saying app works', () => {
+    page.navigateTo()
+    expect(page.getParagraphText()).toEqual('rhythmos Works!');
+  });
+});
