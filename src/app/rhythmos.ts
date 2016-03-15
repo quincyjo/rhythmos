66710d6e547dd558e14050f634e9699def4a108f
RobertCompton <comptonrj@appstate.edu>
2016-03-15 10:42:09 -0400

    Basic song wheel components and services.

--- src/app/rhythmos.ts
+++ src/app/rhythmos.ts
@@ -1,12 +1,13 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {SongWheel} from './components/song-wheel/song-wheel';
 
 
 @Component({
   selector: 'rhythmos-app',
   providers: [],
   templateUrl: 'app/rhythmos.html',
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, SongWheel],
   pipes: []
 })
 @RouteConfig([
@@ -14,7 +15,7 @@ import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 ])
 export class RhythmosApp {
   defaultMeaning: number = 42;
-  
+
   meaningOfLife(meaning?: number) {
     return `The meaning of life is ${meaning || this.defaultMeaning}`;
   }
