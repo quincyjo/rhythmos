ab4562b520a2a9f476f948d1944999f76a55c9ce
Robert Compton <comptonrj@appstate.edu>
2016-04-12 14:07:58 -0500

    Switched song to model provider pair

--- src/app/services/song-provider/song-provider.ts
+++ src/app/services/song-provider/song-provider.ts
@@ -1,14 +1,15 @@
 import {Injectable} from 'angular2/core';
 import {Song} from '../../shared/interfaces/song';
+import {SongModel} from '../../models/song-model';
 import {SONGS} from './mock-songs';
 
 
 @Injectable()
 export class SongProvider {
 
-  constructor() {}
+  constructor(private _model: SongModel) {}
 
   getSongs() {
-    return Promise.resolve(SONGS);
+    return Promise.resolve(this._model.getSongs());
   }
 }
